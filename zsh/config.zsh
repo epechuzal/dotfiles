2fa1171c9cd42838be61874927ed788a069d7f90
Eddy Pechuzal <epechuzal@sharethrough.com>
2022-03-06 14:44:32 -0800

    Use DOTFILES as env var instead of ZSH

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,9 +1,9 @@
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
 
-fpath=($ZSH/functions $fpath)
+fpath=($DOTFILESDIR/functions $fpath)
 
-autoload -U $ZSH/functions/*(:t)
+autoload -U $DOTFILESDIR/functions/*(:t)
 
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
---
+++ zsh/theme.zsh
@@ -0,0 +1,16 @@
+ZSH_THEME_DIR=~/powerlevel10k
+if [ ! -d "$ZSH_THEME_DIR" ]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_THEME_DIR"
+fi
+
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+source $ZSH_THEME_DIR/powerlevel10k.zsh-theme
