668493b521a6cd805754819793bdc64c8e52e2b9
epechuzal <eddy.pechuzal@gmail.com>
2022-10-20 03:14:06 +0000

    Fixes for ubuntu server

--- str/install.sh
+++ str/install.sh
@@ -5,14 +5,10 @@
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
 
-continue_without_install() {
+if [ -z "$INSTALL_STR_BREWFILE" ]; then
   echo "  INSTALL_STR_BREWFILE env var is not set. Will not install STR Brewfile."
-}
-
-install_str_brewfile() {
+else 
   echo "  Installing packages from the STR Brewfile."
 
   brew bundle --file="$DOTFILESDIR/str/Brewfile"
-}
-
-[[ -z "$INSTALL_STR_BREWFILE" ]] && continue_without_install || install_str_brewfile
+fi
